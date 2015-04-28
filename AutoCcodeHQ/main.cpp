eb69fa7ff98dbabd88956b1889871fcf2c625735
yjduoduo <wxjlmr@126.com>
2015-04-28 23:37:11 +0800

    创建数据库 示例 查询服务器当前的参数设置

--- AutoCcodeHQ/main.cpp
+++ AutoCcodeHQ/main.cpp
@@ -2,9 +2,60 @@
 #include <QSqlDatabase>
 #include <QSqlDriver>
 #include <QDebug>
+#include <QSqlTableModel>
+#include <QMessageBox>
+#include <QSqlError>
 
 #include "mainwindow.h"
 #include "libpq-fe.h"
+
+
+/* 创建数据库，failed */
+void CreateDb()
+{
+    QSqlTableModel *sqlModel;
+    QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL","MYPSQLDBCONN");
+    db.setHostName("localhost");
+    db.setDatabaseName("testdb");
+    db.setUserName("postgres");
+    db.setPassword("00000000");
+    db.setPort(5432);
+
+    if(db.open())
+    {
+        // QSqlTableModel 作为数据源
+        sqlModel = new QSqlTableModel(NULL,db);
+        sqlModel->setTable("testtable");
+        sqlModel->setEditStrategy(QSqlTableModel::OnFieldChange);
+        sqlModel->select();
+
+//        // http://ode.cnblogs.com
+//        // http://odevincent.blog.51cto.com
+//        tableView->setModel(sqlModel);
+//        tableView->setSelectionBehavior(QAbstractItemView::SelectRows);
+//        tableView->setAlternatingRowColors(true);
+//        tableView->setGridStyle(Qt::DashLine);
+//        tableView->show();
+    }
+    else
+    {
+        QMessageBox::critical(NULL,"error",db.lastError().text());
+    }
+}
+
+/* 查询服务器当前的参数设置 */
+void PrintPQparaInfo(PGconn *conn)
+{
+    qDebug() << "server_version : " << PQparameterStatus(conn,"server_version");
+    qDebug() << "server_encoding : " << PQparameterStatus(conn,"server_encoding");
+    qDebug() << "client_encoding : " << PQparameterStatus(conn,"client_encoding");
+    qDebug() << "session_authorization : " << PQparameterStatus(conn,"session_authorization");
+    qDebug() << "DateStyle : " << PQparameterStatus(conn,"DateStyle");
+    qDebug() << "TimeZone : " << PQparameterStatus(conn,"TimeZone");
+    qDebug() << "integer_datetimes : " << PQparameterStatus(conn,"integer_datetimes");
+    qDebug() << "standard_conforming_strings : " << PQparameterStatus(conn,"standard_conforming_strings");
+}
+
 
 #if 1
 int main(int argc, char *argv[])
@@ -18,13 +69,17 @@ int main(int argc, char *argv[])
     {
         printf("connect db failed...\n");
         PQfinish(conn);
-       return a.exec();
+        return a.exec();
     }
 
-    QStringList drivers = QSqlDatabase::drivers();
-        foreach(QString driver, drivers)
-            qDebug()<<driver;
+    PrintPQparaInfo(conn);
 
+
+    QStringList drivers = QSqlDatabase::drivers();
+    foreach(QString driver, drivers)
+        qDebug()<<driver;
+
+//    CreateDb();
 
     printf("connect db successfull...\n");
 
@@ -49,7 +104,7 @@ int main(int argc, char *argv[])
     {
         printf("connect db failed...\n");
         PQfinish(conn);
-       return a.exec();
+        return a.exec();
     }
     printf("connect db successfull...\n");
 
